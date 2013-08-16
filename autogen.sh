84ae3c0faf0f59a67f92bcaeb15e788b7f49d8de
jimmycao <jimmycao@emc.com>
2013-08-16 15:25:09 +0800

    add v1.6, passed

--- autogen.sh
+++ autogen.sh
@@ -1563,7 +1563,7 @@ if test "x$config" = "x" -o ! -f "$config" ; then
     $ECHO "misconfiguration."
 else
     $ECHO "The $PROJECT build system is now prepared.  To build here, run:"
-    $ECHO "  $config --prefix=/where/open-mpi/installed"
+    $ECHO "  $config --prefix=/where/open-mpi/installed --ompi-version=[OpenMPI Version, e.g., 1.7.2]"
     $ECHO "  make"
 fi
 
